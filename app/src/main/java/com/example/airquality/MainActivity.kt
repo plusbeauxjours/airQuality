8d8c95bd6e835c9318f7cae88efc1a1a2bcbd516
minjaeli <minjaelee@comcom.ai>
2023-08-28 19:56:08 +0700

    set: Location permission

--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.airquality
 
 import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.airquality.databinding.ActivityMainBinding
@@ -19,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
+
+    lateinit var getGPSPermissionLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,8 +65,10 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        var checkResult = true
+
         if(requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size){
-            var checkResult = true
+
             for (result in grantResults){
                 if(result != PackageManager.PERMISSION_GRANTED){
                     checkResult = false
@@ -71,5 +82,36 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show()
             finish()
         }
+    }
+
+    private fun showDialogForLocationServiceSetting(){
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){result->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(isLocationServicesAvailabe()){
+                    isRunTimePermissionsGranted()
+                }else{
+                    Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져있습니다. 설정해야 앱을 사용할 수 있습니다.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialogInterface, i ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener{dialogInterface, i->
+            dialogInterface.cancel()
+            Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+            finish()
+        })
+
+        builder.create().show()
     }
 }
